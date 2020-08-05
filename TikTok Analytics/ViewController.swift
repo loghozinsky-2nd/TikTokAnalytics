87b8ae9ae523730b982fdc7b9bf25c19de286c1e
loghozinsky <loghozinsky@gmail.com>
2020-08-05 09:46:12 +0300

    Initial Commit

---
+++ TikTok Analytics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTok Analytics
+//
+//  Created by Aleksander Logozinsky on 05.08.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
